9c0050de6878a7c9b8ad1907d6d1f356c62a2742
Raven Ackerman <rayvayvo@gmail.com>
2017-04-14 15:44:03 +0000

    added admin create category function

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,37 @@
+
+
+
+class Admin::CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all.order(created_at: :desc)
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    @products = @category.products.order(created_at: :desc)
+  end
+
+def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+   private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+    )
+  end
+
+end
